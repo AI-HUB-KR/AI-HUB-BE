43327e550aa477cde6a204e25c329f9bfe4407ee
chabin37 <7chabin@gmail.com>
2025-09-22 20:16:42 +0900

    Initial commit: Spring project setup

---
+++ src/main/java/kr/ai_hub/AI_HUB_BE/AiHubBeApplication.java
@@ -0,0 +1,13 @@
+package kr.ai_hub.AI_HUB_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHubBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiHubBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ai_hub/AI_HUB_BE/AiHubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ai_hub.AI_HUB_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHubBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
