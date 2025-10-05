84bb74d48d5364c18094d059dc473c1c44641fb3
chabin37 <7chabin@gmail.com>
2025-10-06 00:23:07 +0900

    feat : Entity와 JPA를 활용한 Repository 설계

---
+++ src/main/java/kr/ai_hub/AI_HUB_BE/domain/aimodel/repository/AIModelRepository.java
@@ -0,0 +1,18 @@
+package kr.ai_hub.AI_HUB_BE.domain.aimodel.repository;
+
+import kr.ai_hub.AI_HUB_BE.domain.aimodel.entity.AIModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AIModelRepository extends JpaRepository<AIModel, Integer> {
+
+    Optional<AIModel> findByModelName(String modelName);
+
+    List<AIModel> findByIsActive(Boolean isActive);
+
+    List<AIModel> findByIsActiveTrueOrderByCreatedAtDesc();
+}
