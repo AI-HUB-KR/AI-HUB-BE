579f553a78cc64f20b0d93e88f56135fdcf7ed2f
chabin37 <7chabin@gmail.com>
2025-12-05 22:49:57 +0900

    fix: delete 메서드를 위한 List 호출 추가

--- src/main/java/kr/ai_hub/AI_HUB_BE/application/auth/refreshtoken/RefreshTokenService.java
+++ src/main/java/kr/ai_hub/AI_HUB_BE/application/auth/refreshtoken/RefreshTokenService.java
@@ -20,6 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Service
